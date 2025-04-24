f3a194227ceb60ffe300305d01962e0538da0b48
Akash <hafejmohammadakash@gmail.com>
2025-04-24 11:07:51 +0600

    simple ekta fromer prathomik structure banalam

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <SimpleForm />
+    </>
+  );
+}
+
+export default App;
